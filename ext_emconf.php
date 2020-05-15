10d383a0d45461f16bb9227a946e70de17a970df
Tobias Schmidt <t.schmidt@minuskel.de>
2020-05-15 15:10:33 +0200

    [FEATURE] Raise supported TYPO3 and PHP versions

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,8 +10,8 @@ $EM_CONF[$_EXTKEY] = [
   'version' => '2.1.0',
   'constraints' => [
     'depends' => [
-      'php' => '7.0.0-7.3.99',
-      'typo3' => '8.7.0-9.99.99',
+      'php' => '7.2.0-7.4.99',
+      'typo3' => '10.4.0-10.4.99',
     ],
     'conflicts' => [],
     'suggests' => [],
