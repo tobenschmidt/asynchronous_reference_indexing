0aa03f71abe2536f5c23c04c897443a3d6722fe3
Tobias Schmidt <t.schmidt@minuskel.de>
2020-07-08 13:30:29 +0200

    [RELEASE] 3.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
   'title' => 'Asynchronous Reference Indexing',
-  'description' => 'Delegates reference indexing to a Symfony Console Command (scheduler compatible) to avoid major performance issues on very large setups or large database operations.',
+  'description' => 'Delegates reference indexing to a Symfony Console Command (scheduler compatible) to avoid major performance issues on very large setups or large database operations. (TYPO3 10 compatibility by Toben Schmidt)',
   'category' => 'misc',
   'author' => 'Claus Due',
   'author_email' => 'claus@namelesscoder.net',
   'author_company' => '',
   'state' => 'beta',
-  'version' => '2.1.0',
+  'version' => '3.0.0',
   'constraints' => [
     'depends' => [
       'php' => '7.2.0-7.4.99',
